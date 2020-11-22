48b9336e1b092f71f52b2563fa9c10905c6a68d7
Dlabins <DLABINS1895@GMAIL.COM>
2020-11-21 19:02:44 -0500

    changed #descriptions to #description to avoid undefined output into readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,7 @@ function generateMarkdown(data) {
   - [Questions](#questions)
 
   ## Description
-  ${data.descriptions}
+  ${data.description}
   ## Installation
   ${data.installation}
   ## Usage
